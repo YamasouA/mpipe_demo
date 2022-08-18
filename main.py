6beeda3476366876d566812f7df877d094611f8d
YamasouA <akiakiskyhand@gmail.com>
2022-08-18 22:30:38 +0900

    add function

--- main.py
+++ main.py
@@ -7,6 +7,53 @@ def dist(lnd1, lnd2):
 	return ((lnd1[0] - lnd2[0]) ** 2 + (lnd1[1] - lnd2[1]) ** 2) ** 0.5
 	
 
+def check_hand_pose(hand1, hand2):
+	thumb1 = np.array(hand1[4])
+	pointer1 = np.array(hand1[8])
+	corner1 = np.array(hand1[0])
+
+	thumb2 = np.array(hand2[4])
+	pointer2 = np.array(hand2[8])
+	corner2 = np.array(hand2[0])
+
+	vector1_1 = thumb1 - corner1
+	vector1_2 = pointer1 - corner1
+
+	vector2_1 = thumb2 - corner2
+	vector2_2 = pointer2 - corner2
+
+	i1 = np.inner(vector1_1, vector1_2)
+	i2 = np.inner(vector2_1, vector2_2)
+	n1 = np.linalg.norm(vector1_1) * np.linalg.norm(vector1_2)
+	n2 = np.linalg.norm(vector2_1) * np.linalg.norm(vector2_2)
+	c1 = i1 / n1
+	c2 = i2 / n2
+	ang1 = np.rad2deg(np.arccos(np.clip(c1, -1.0, 1.0)))
+	ang2 = np.rad2deg(np.arccos(np.clip(c2, -1.0, 1.0)))
+	print("angle1: ", ang1, "\n")
+	print("angle2: ", ang2, "\n")
+	if ang1 > 45 and ang2 > 45:
+		return True
+	return False
+
+def draw_stealth(img, hand1, hand2):
+	bg_img = cv2.imread('./bg.png')
+	zoom_size_x = int(abs(hand1[0][0] - hand2[0][0]))
+	zoom_size_y = int(abs(hand1[0][1] - hand2[0][1]))
+
+	print("zoom_size_x: ", zoom_size_x, "\n")
+	print("zoom_size_y: ", zoom_size_y, "\n")
+	try:
+		print(img.shape)
+		new_bg = cv2.resize(bg_img, dsize=(img.shape[1], img.shape[0]))
+		pos_x = int(min(hand1[0][0], hand2[0][0]))
+		pos_y = int(min(hand1[0][1], hand2[0][1]))
+		cut_bg = new_bg[pos_y: pos_y + zoom_size_y, pos_x: pos_x + zoom_size_x]
+		img[pos_y: pos_y + zoom_size_y, pos_x: pos_x + zoom_size_x] = cut_bg
+	except:
+		print("error")
+	return img
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
@@ -84,56 +131,70 @@ with mp_hands.Hands(
       lnd_list = []
       #print("\n ========================================\n\n")
       #print("len: ", len(results.multi_hand_landmarks))
+      hand1, hand2= [], []
+      hand_cnt = 0
       for hand_landmarks in results.multi_hand_landmarks:
-        for id, lm in enumerate(hand_landmarks.landmark):
-          lnd_list.append([lm.x * image_width, lm.y * image_height])
-        #print(lnd_list)
-        if dist(lnd_list[4], lnd_list[8]) < image_width / 40:
-          print(i, "\n")
-          i+=1
-          print("here\n\n")
-          zoom_flag = True
-        if zoom_flag:
-          center_x = int((lnd_list[4][0] + lnd_list[8][0]) / 2)
-          center_y = int((lnd_list[4][1] + lnd_list[8][1]) / 2)
-          dist_x = int(abs(lnd_list[4][0] - lnd_list[8][0]))
-          dist_y = int(abs(lnd_list[4][1] - lnd_list[8][1]))
-          dist_x += 10
-          dist_y += 10
-          dist_x, dist_y = max(dist_x, dist_y), max(dist_x, dist_y)
-          print("dist_y: ", dist_y)
-          print("dist_x: ", dist_x)
+        hand_num = len(results.multi_hand_landmarks)
+        if hand_num == 1:
+            for id, lm in enumerate(hand_landmarks.landmark):
+              lnd_list.append([lm.x * image_width, lm.y * image_height])
+            #print(lnd_list)
+            if dist(lnd_list[4], lnd_list[8]) < image_width / 40:
+              print(i, "\n")
+              i+=1
+              print("here\n\n")
+              zoom_flag = True
+            if zoom_flag:
+              center_x = int((lnd_list[4][0] + lnd_list[8][0]) / 2)
+              center_y = int((lnd_list[4][1] + lnd_list[8][1]) / 2)
+              dist_x = int(abs(lnd_list[4][0] - lnd_list[8][0]))
+              dist_y = int(abs(lnd_list[4][1] - lnd_list[8][1]))
+              dist_x += 10
+              dist_y += 10
+              dist_x, dist_y = max(dist_x, dist_y), max(dist_x, dist_y)
+              print("dist_y: ", dist_y)
+              print("dist_x: ", dist_x)
 
-          
-          print("center_x, center_y: ", center_x, center_y)
-          if ((center_x + 50 > image_width or center_y + 50 > image_height) or
-	    (center_x - 50 < 0 or center_y - 50 < 0)):
-            continue
-          else:
-            new_img = image[center_y - 50: center_y + 50, center_x - 50: center_x + 50]
-            new_img = cv2.resize(new_img, dsize=(dist_x, dist_y))
-          rnd = 0
-          if dist_y % 2 == 0:
-            rang_y = int(dist_y / 2)
-          else:
-            rang_y = int(dist_y / 2)
-            rnd = 1
-          if dist_x % 2 == 0:
-            rang_x = int(dist_x / 2)
-          else:
-            rang_x = int(dist_x / 2)
-            rnd = 1
-          print("new_img.shape: ", new_img.shape)
-          print("rang_x: ",center_x + rang_x + rnd)
-          print("rang_x: ",rang_x)
-          print("dist_x: ",dist_x)
-          print("dist_y: ",dist_y)
-          if ((center_y + rang_y + rnd > image_height or center_x + rang_x + rnd > image_width)
-	    or (center_y - rang_y < 0 or center_x - rang_x < 0)):
-            continue
-          else:
-            image[center_y - rang_y: center_y + rang_y + rnd, center_x - rang_x: center_x + rang_x + rnd] = new_img
-          #cv2.imshow("a", new_img)
+              
+              print("center_x, center_y: ", center_x, center_y)
+              if ((center_x + 50 > image_width or center_y + 50 > image_height) or
+	        (center_x - 50 < 0 or center_y - 50 < 0)):
+                continue
+              else:
+                new_img = image[center_y - 50: center_y + 50, center_x - 50: center_x + 50]
+                new_img = cv2.resize(new_img, dsize=(dist_x, dist_y))
+              rnd = 0
+              if dist_y % 2 == 0:
+                rang_y = int(dist_y / 2)
+              else:
+                rang_y = int(dist_y / 2)
+                rnd = 1
+              if dist_x % 2 == 0:
+                rang_x = int(dist_x / 2)
+              else:
+                rang_x = int(dist_x / 2)
+                rnd = 1
+              print("new_img.shape: ", new_img.shape)
+              print("rang_x: ",center_x + rang_x + rnd)
+              print("rang_x: ",rang_x)
+              print("dist_x: ",dist_x)
+              print("dist_y: ",dist_y)
+              if ((center_y + rang_y + rnd > image_height or center_x + rang_x + rnd > image_width)
+	        or (center_y - rang_y < 0 or center_x - rang_x < 0)):
+                continue
+              else:
+                image[center_y - rang_y: center_y + rang_y + rnd, center_x - rang_x: center_x + rang_x + rnd] = new_img
+              #cv2.imshow("a", new_img)
+        elif hand_num == 2:
+            if hand_cnt == 0:
+                for id, lm in enumerate(hand_landmarks.landmark):
+                  hand1.append([lm.x * image_width, lm.y * image_height])
+            else:
+                for id, lm in enumerate(hand_landmarks.landmark):
+                  hand2.append([lm.x * image_width, lm.y * image_height])
+                if check_hand_pose(hand1, hand2):
+                    image = draw_stealth(image, hand1, hand2)
+            hand_cnt += 1
         #print("x: ", hand_landmarks[4])
         #print("x: ", hand_landmarks[4])
         #print("x: ", hand_landmarks[4])
